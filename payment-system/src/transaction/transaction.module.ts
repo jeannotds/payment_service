5f7314d8c7a3a480e3e79f7b30ab80dcabe07ff6
Jeannot Diambu <diambujeannot@gmail.com>
2026-05-20 13:41:07 +0100

    fix: error import and export module wallet

--- payment-system/src/transaction/transaction.module.ts
+++ payment-system/src/transaction/transaction.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
   providers: [TransactionService, PrismaService],
   controllers: [TransactionController],
+  imports: [WalletModule],
 })
 export class TransactionModule {}
