f5d9f8cfbdbd463f3a759b55f8de429351073d04
Jeannot Diambu <diambujeannot@gmail.com>
2026-05-21 19:32:09 +0100

    create project frontend

--- payment-system/src/main.ts
+++ payment-system/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(',') ?? [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
