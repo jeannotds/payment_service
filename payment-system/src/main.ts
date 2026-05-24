4359178f1c8b6394d5e294f419ccbbb1b416a23d
Jeannot Diambu <diambujeannot@gmail.com>
2026-05-24 21:21:15 +0100

    add hermet

--- payment-system/src/main.ts
+++ payment-system/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,9 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
