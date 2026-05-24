0b10ec1f0628052d6225605ceaff7117545ec3da
Jeannot Diambu <diambujeannot@gmail.com>
2026-05-24 20:10:07 +0100

    add validation pipe

--- payment-system/src/main.ts
+++ payment-system/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Supprime les propriétés non définies dans le DTO
+      forbidNonWhitelisted: true, // Rejette les requêtes avec des propriétés non définies dans le DTO
+      transform: true, // Transforme les données en fonction des DTOs
+    }),
+  );
   app.enableCors({
     origin: process.env.CORS_ORIGIN?.split(',') ?? [
       'http://localhost:3000',
